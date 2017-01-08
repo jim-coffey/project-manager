7c3ac68b96459ce778328b43dd965dd266e749e7
Jim Coffey <jim_coffey@hotmail.com>
2017-01-08 11:40:40 +0000

    Doctrine ORM

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-mava
-====
+Task Manager
+=====
 
 A Symfony project created on January 7, 2017, 2:01 pm.
--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -5,25 +5,37 @@ namespace AppBundle\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
+use AppBundle\Entity\User;
 
 class DefaultController extends Controller
 {
-    /**
-     * @Route("/", name="homepage")
-     */
-    public function indexAction(Request $request)
-    {
-        // replace this example code with whatever you need
-        return $this->render('default/index.html.twig', [
-            'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
-        ]);
+  /**
+    * @Route("/", name="homepage")
+    */
+  public function indexAction(Request $request)
+  {
+    // replace this example code with whatever you need
+    return $this->render('default/index.html.twig', [
+      'base_dir' => realpath($this->getParameter('kernel.root_dir').'/..').DIRECTORY_SEPARATOR,
+    ]);
+  }
+
+  /**
+    * @Route("/about/{name}", name="aboutpage", defaults={"name": null})
+    */
+  public function aboutAction($name)
+  {
+    if ($name) {
+      $user = $this->getDoctrine()
+                   ->getRepository('AppBundle:User')
+                   ->findOneBy(array('name'=>$name));
+      if (false === $user instanceof User) {
+        throw $this->createNotFoundException(
+          'No user named ' . $name . ' found!'
+        );
+      }
     }
 
-    /**
-     * @Route("/about/{name}", name="aboutpage", defaults={"name": null})
-     */
-    public function aboutAction($name)
-    {
-        return $this->render('about/index.html.twig', array('name' => $name));
-    }
+    return $this->render('about/index.html.twig', array('user' => $user));
+  }
 }
